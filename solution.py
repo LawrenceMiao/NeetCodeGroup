36aeb8fe046fbbb4811c62176077ac304a5692a9
Lawrence M <lawrencejunzh@gmail.com>
2024-06-15 01:14:04 -0400

    asdfas

--- solution.py
+++ solution.py
@@ -3,6 +3,6 @@ from typing import List
 
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
-        for i in range(800010):
+        for i in range(800110):
             pass
         return 5
